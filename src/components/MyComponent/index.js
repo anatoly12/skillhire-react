381788aadb8fbfdc54dffda140eed1eb3eb757f0
Anatoly P <anatolypolicastroa@gmail.com>
2019-11-01 22:55:56 -0400

    update(app): complete todo list

--- src/components/MyComponent/index.js
+++ src/components/MyComponent/index.js
@@ -1,14 +1,56 @@
-import React from 'react'
-import { Button } from '@material-ui/core'
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
+
+import CustomList from './CustomList';
 
 /*
   Welcome developer! Please fully customize this component as needed.
 */
+const useStyles = makeStyles(theme => ({
+  container: {
+    width: '100%'
+  },
+  textField: {
+    width: '40%',
+    padding: theme.spacing(1),
+    display: 'block',
+    width: 200,
+    color: 'rgba(0, 0, 0, 0.87)'
+  }
+}));
 
-const MyComponent = () => (
-  <Button>
-    Click Me
-  </Button>
-)
+const MyComponent = () => {
+  const classes = useStyles();
 
-export default MyComponent
+  const [list, setList] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const handleChange = e => {
+    e.preventDefault();
+
+    setInputText(e.target.value);
+  };
+
+  const handleClickEnter = e => {
+    if (e.key === 'Enter' && !!inputText) {
+      setList([inputText, ...list]);
+      setInputText('');
+    }
+  };
+
+  return (
+    <div className={classes.container} noValidate autoComplete="off">
+      <TextField
+        className={classes.textField}
+        margin="normal"
+        value={inputText}
+        onChange={handleChange}
+        onKeyDown={handleClickEnter}
+        placeholder="Enter Todo..."
+      />
+      <CustomList list={list} />
+    </div>
+  );
+};
+export default MyComponent;
